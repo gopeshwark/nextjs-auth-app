fd99420ee49c24e386cbea952e7086f30157ff5c
gopeshwark <gopeshwark@gmail.com>
2023-10-15 10:46:21 +0530

    Completed login functinality

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -6,47 +6,48 @@ import jwt from 'jsonwebtoken';
 
 connect();
 
-export async function POST(request:NextRequest){
+export async function POST(request: NextRequest) {
     try {
-        const reqBody = request.json();
-        const {email, password} = reqBody;
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
         console.log(reqBody)
 
         // check if user exists
-        const user = await User.findOne({email});
-        if(!user){
-            return NextResponse.json({error:"User does not exists"}, {status:409});
+        const user = await User.findOne({ email });
+        console.log(user)
+        if (!user) {
+            return NextResponse.json({ error: "User does not exists" }, { status: 409 });
         }
 
         // check if password is correct
         const validPassword = await bcryptjs.compare(password, user.password);
-        if(!validPassword){
-            return NextResponse.json({error:"Invalid Password!"}, {status:401});
+        if (!validPassword) {
+            return NextResponse.json({ error: "Invalid Password!" }, { status: 401 });
         }
 
         // create token data
         const tokenData = {
             id: user._id,
-            username:user.username,
+            username: user.username,
             email: user.email
         }
 
         // create token
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn:"1h"});
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1h" });
 
         // create response
         const response = NextResponse.json({
-            message:"Login successful",
+            message: "Login successful",
             success: true,
         })
 
         response.cookies.set("token", token, {
-            httpOnly:true,
+            httpOnly: true,
         })
 
         return response;
 
     } catch (error: any) {
-        return NextResponse.json({error:error.messgae},{status:500});
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,20 +3,48 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast/headless";
 
 const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   const onLogin = async () => {
-    console.log(user);
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      if(response.data.success){
+        console.log("Login success", response.data);
+        toast.success("Login success");
+        router.push("/profile");
+      } else {
+        console.log("Login failed");
+        toast.error("Login failed")
+      }
+    } catch (error: any) {
+        console.log("Login failed", error.message);
+        toast.error(error.messate)
+    } finally {
+      setLoading(false);
+    }
   };
+
+  React.useEffect(() => {
+    if(user.email && user.password){
+      setButtonDisabled(false)
+    } else{
+      setButtonDisabled(true);
+    }
+  },[user])
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
         <label htmlFor="email">Email</label>
         <input
@@ -38,7 +66,7 @@ const LoginPage = () => {
           onChange={(e) => setUser({ ...user, password: e.target.value })}
           placeholder="password"
         />
-      <button onClick={onLogin} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login</button>
+      <button onClick={onLogin} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No Login" : "Login"}</button>
       <Link href="/signup">Visit signup page</Link>
     </div>
   );

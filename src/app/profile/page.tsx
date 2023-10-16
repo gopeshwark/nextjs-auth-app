734c6476d193b0e8207953a34a8813ec041761a3
gopeshwark <gopeshwark@gmail.com>
2023-10-16 13:05:28 +0530

    Added user route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,31 +1,48 @@
 "use client";
 
-import axios from 'axios';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import {toast} from 'react-hot-toast';
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "react-hot-toast";
 
 const ProfilePage = () => {
   const router = useRouter();
-  const logout =async () => {
+  const [user, setUser] = React.useState("");
+  const logout = async () => {
     try {
       await axios.get("/api/users/logout");
       toast.success("Logged out successfully!");
       router.push("/login");
-      
     } catch (error: any) {
       console.log(error.message);
       toast.error(error.message);
     }
-  }
+  };
+
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/user");
+    console.log(response.data);
+    setUser(response.data.data._id);
+    router.push(`/profile/${response.data.data._id}`)
+  };
+
+  React.useEffect(() => {
+    getUserDetails();
+  },[])
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       ProfilePage
-      <hr/>
-      <button onClick={logout} className='bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Logout</button>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
